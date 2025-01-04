481de4ce896fca9679185c0f026fa39e739fe48f
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2025-01-04 10:49:43 +0000

    update config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 module.exports = {
-    TELEGRAM_BOT_TOKEN: '6878912917:AAGGPxYGh5Nx5vxqI6PD5luQpKsgGTWhegE',
+    TELEGRAM_BOT_TOKEN: '7941410758:AAGV_PLX83E9iYf_A5dCl4F82aPRp4qYBrk',
   };
   
