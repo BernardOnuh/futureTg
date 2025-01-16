0d27720547ffadc620fb213ca9e3036631a5265d
BernardOnuh <onuhbernard4@gmail.com>
2025-01-16 17:42:05 +0100

    future edge

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
-  TELEGRAM_BOT_TOKEN: '7878828351:AAEAQ02qYfIfVBDMmeeujFzih9TUw2ahYlI',
-  EVA_API_KEY: '574de7e0c1a4c8ad291ed7e8cd1d6ed9',
-  EVA_API_BASE_URL: 'https://api.eva-ai.cloud',
-  BASE_URL: 'https://fets-database.onrender.com/api/future-edge',
-  ALCHEMY_API_KEY: 'nRKZNN7FV_lFECaCuzF1jGPPkcCD8ogi',
-  QUICKNODE_BSC_URL: 'https://quaint-neat-cherry.bsc.quiknode.pro/6bca20cae39a942e13525f4048fc30211405a7b5/'
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  EVA_API_KEY: process.env.EVA_API_KEY,
+  EVA_API_BASE_URL: process.env.EVA_API_BASE_URL,
+  BASE_URL: process.env.BASE_URL,
+  ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+  QUICKNODE_BSC_URL: process.env.QUICKNODE_BSC_URL
 };
